3e1759d48fe9019f2591efeed7ecad335322c1c3
Muhammad <hamidbajwa76@gmail.com>
2023-02-21 22:50:24 +0500

    final

--- src/Component/AboutComponent.js
+++ src/Component/AboutComponent.js
@@ -3,12 +3,12 @@ import React from "react";
 function About() {
   return (
     <>
-      <div class="">
-        <div class="container py-5">
-          <div class="row h-100 align-items-center py-5">
-            <div class="col-lg-6">
-              <h1 class="display-4">About us</h1>
-              <p class="lead  mb-0" style={ {color :"#c2cad0"} }>
+      <div className="">
+        <div className="container py-5">
+          <div className="row h-100 align-items-center py-5">
+            <div className="col-lg-6">
+              <h1 className="display-4">About us</h1>
+              <p className="lead  mb-0" style={ {color :"#c2cad0"} }>
                 Welcome to our platform, a comprehensive resource for students
                 looking to find scholarships in Punjab, Pakistan. Our goal is to
                 help students achieve their academic dreams by providing them
@@ -16,24 +16,24 @@ function About() {
                 their education.
               </p>
             </div>
-            <div class="col-lg-6 d-none d-lg-block">
+            <div className="col-lg-6 d-none d-lg-block">
               <img
                 src="https://bootstrapious.com/i/snippets/sn-about/illus.png"
                 alt=""
-                class="img-fluid"
+                className="img-fluid"
               />
             </div>
           </div>
         </div>
       </div>
 
-      <div class="py-5">
-        <div class="container py-5">
-          <div class="row align-items-center mb-5">
-            <div class="col-lg-6 order-2 order-lg-1">
-              <i class="fa fa-bar-chart fa-2x mb-3 text-primary"></i>
-              <h2 class="font-weight-light">Our Mission and Goal</h2>
-              <p class="font-italic mb-4" style={ {color :"#c2cad0"} }>
+      <div className="py-5">
+        <div className="container py-5">
+          <div className="row align-items-center mb-5">
+            <div className="col-lg-6 order-2 order-lg-1">
+              <i className="fa fa-bar-chart fa-2x mb-3 text-primary"></i>
+              <h2 className="font-weight-light">Our Mission and Goal</h2>
+              <p className="font-italic mb-4" style={ {color :"#c2cad0"} }>
                 We understand that the cost of education can be a major barrier
                 for many students, particularly in a country like Pakistan where
                 many families struggle to make ends meet. That's why we have
@@ -42,26 +42,26 @@ function About() {
                 expenses.
               </p>
             </div>
-            <div class="col-lg-5 px-5 mx-auto order-1 order-lg-2">
+            <div className="col-lg-5 px-5 mx-auto order-1 order-lg-2">
               <img
                 src="https://bootstrapious.com/i/snippets/sn-about/img-1.jpg"
                 alt=""
-                class="img-fluid mb-4 mb-lg-0"
+                className="img-fluid mb-4 mb-lg-0"
               />
             </div>
           </div>
-          <div class="row align-items-center">
-            <div class="col-lg-5 px-5 mx-auto">
+          <div className="row align-items-center">
+            <div className="col-lg-5 px-5 mx-auto">
               <img
                 src="https://bootstrapious.com/i/snippets/sn-about/img-2.jpg"
                 alt=""
-                class="img-fluid mb-4 mb-lg-0"
+                className="img-fluid mb-4 mb-lg-0"
               />
             </div>
-            <div class="col-lg-6">
-              <i class="fa fa-leaf fa-2x mb-3 text-primary"></i>
-              <h2 class="font-weight-light">Comprehensive Scholarship Database and Support</h2>
-              <p class="font-italic  mb-4" style={ {color :"#c2cad0"} }> 
+            <div className="col-lg-6">
+              <i className="fa fa-leaf fa-2x mb-3 text-primary"></i>
+              <h2 className="font-weight-light">Comprehensive Scholarship Database and Support</h2>
+              <p className="font-italic  mb-4" style={ {color :"#c2cad0"} }> 
                 We believe that every student should have access to the
                 resources they need to achieve their academic goals, regardless
                 of their financial background. We are committed to helping
@@ -76,39 +76,39 @@ function About() {
         </div>
       </div>
 
-      <div class="py-5">
-        <div class="container py-5">
-          <div class="row mb-4">
-            <div class="col-lg-5">
-              <h2 class="display-4 font-weight-light">Our team</h2>
+      <div className="py-5">
+        <div className="container py-5">
+          <div className="row mb-4">
+            <div className="col-lg-5">
+              <h2 className="display-4 font-weight-light">Our team</h2>
             </div>
           </div>
 
-          <div class="row text-center justify-content-center">
+          <div className="row text-center justify-content-center">
 
             {/* <!-- Team item 1--> */}
-            <div class="col-xl-3 col-sm-6 mb-5">
-              <div class="bg-white rounded shadow-sm py-5 px-4">
+            <div className="col-xl-3 col-sm-6 mb-5">
+              <div className="bg-white rounded shadow-sm py-5 px-4">
                 <img
                   src="https://bootstrapious.com/i/snippets/sn-about/avatar-3.png"
                   alt=""
                   width="100"
-                  class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"
+                  className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"
                 />
-                <h5 class="mb-0" style={ {color :"#000"} }>Hamid Bajwa</h5>
-                <span class="small text-uppercase text-muted">
+                <h5 className="mb-0" style={ {color :"#000"} }>Hamid Bajwa</h5>
+                <span className="small text-uppercase text-muted">
                   Web Developer
                 </span>
-                <ul class="social mb-0 list-inline mt-3">
+                <ul className="social mb-0 list-inline mt-3">
                  
-                  <li class="list-inline-item">
-                    <a href="https://www.instagram.com/muhammad_hamid__/"  target="_blank"  class="social-link">
-                      <i class="fa fa-instagram"></i>
+                  <li className="list-inline-item">
+                    <a href="https://www.instagram.com/muhammad_hamid__/"  target="_blank"  className="social-link">
+                      <i className="fa fa-instagram"></i>
                     </a>
                   </li>
-                  <li class="list-inline-item">
-                    <a href="https://linkedin.com/in/hamidbajwa/"  target="_blank" class="social-link">
-                      <i class="fa fa-linkedin"></i>
+                  <li className="list-inline-item">
+                    <a href="https://linkedin.com/in/hamidbajwa/"  target="_blank" className="social-link">
+                      <i className="fa fa-linkedin"></i>
                     </a>
                   </li>
                 </ul>
@@ -117,28 +117,28 @@ function About() {
             {/* <!-- End--> */}
 
             {/* <!-- Team item--> */}
-            <div class="col-xl-3 col-sm-6 mb-5">
-              <div class="bg-white rounded shadow-sm py-5 px-4">
+            <div className="col-xl-3 col-sm-6 mb-5">
+              <div className="bg-white rounded shadow-sm py-5 px-4">
                 <img
                   src="https://bootstrapious.com/i/snippets/sn-about/avatar-2.png"
                   alt=""
                   width="100"
-                  class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"
+                  className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"
                 />
-               <h5 class="mb-0" style={ {color :"#000"} }>M.Faizan </h5>
-                <span class="small text-uppercase text-muted">
+               <h5 className="mb-0" style={ {color :"#000"} }>M.Faizan </h5>
+                <span className="small text-uppercase text-muted">
                   Web Developer
                 </span>
-                <ul class="social mb-0 list-inline mt-3">
+                <ul className="social mb-0 list-inline mt-3">
                  
-                  <li class="list-inline-item">
-                    <a href="https://www.instagram.com/_raobilal/"  target="_blank"  class="social-link">
-                      <i class="fa fa-instagram"></i>
+                  <li className="list-inline-item">
+                    <a href="https://www.instagram.com/_raobilal/"  target="_blank"  className="social-link">
+                      <i className="fa fa-instagram"></i>
                     </a>
                   </li>
-                  <li class="list-inline-item">
-                    <a href="https://linkedin.com/in/hamidbajwa/"  target="_blank" class="social-link">
-                      <i class="fa fa-linkedin"></i>
+                  <li className="list-inline-item">
+                    <a href="https://linkedin.com/in/hamidbajwa/"  target="_blank" className="social-link">
+                      <i className="fa fa-linkedin"></i>
                     </a>
                   </li>
                 </ul>
@@ -147,28 +147,28 @@ function About() {
             {/* <!-- End--> */}
 
             {/* <!-- Team item--> */}
-            <div class="col-xl-3 col-sm-6 mb-5">
-              <div class="bg-white rounded shadow-sm py-5 px-4">
+            <div className="col-xl-3 col-sm-6 mb-5">
+              <div className="bg-white rounded shadow-sm py-5 px-4">
                 <img
                   src="https://bootstrapious.com/i/snippets/sn-about/avatar-1.png"
                   alt=""
                   width="100"
-                  class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"
+                  className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"
                 />
-                <h5 class="mb-0" style={ {color :"#000"} }>Bilal Sami</h5>
-                <span class="small text-uppercase text-muted">
+                <h5 className="mb-0" style={ {color :"#000"} }>Bilal Sami</h5>
+                <span className="small text-uppercase text-muted">
                   Web Developer
                 </span>
-                <ul class="social mb-0 list-inline mt-3">
+                <ul className="social mb-0 list-inline mt-3">
                   
-                  <li class="list-inline-item">
-                    <a href="https://www.instagram.com/eff.dot/"  target="_blank"  class="social-link">
-                      <i class="fa fa-instagram"></i>
+                  <li className="list-inline-item">
+                    <a href="https://www.instagram.com/eff.dot/"  target="_blank"  className="social-link">
+                      <i className="fa fa-instagram"></i>
                     </a>
                   </li>
-                  <li class="list-inline-item">
-                    <a href="https://www.linkedin.com/in/muhammad-faizan-530688210/"  target="_blank" class="social-link">
-                      <i class="fa fa-linkedin"></i>
+                  <li className="list-inline-item">
+                    <a href="https://www.linkedin.com/in/muhammad-faizan-530688210/"  target="_blank" className="social-link">
+                      <i className="fa fa-linkedin"></i>
                     </a>
                   </li>
                 </ul>
